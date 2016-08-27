aa12d5800dcd49bfe0b318f6dbe56693168eedc5
Chris Butcher <christopherbutcher@live.com>
2016-08-27 14:48:48 +0100

    Transactional slider

--- src/containers/transaction_slider.js
+++ src/containers/transaction_slider.js
@@ -3,6 +3,7 @@ import React, {
 } from 'react';
 import {
   View,
+  Slider,
   Text,
   StyleSheet
 } from 'react-native'
@@ -13,22 +14,50 @@ class TransactionSlider extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      numberOfShares: 0
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>'This is the transaction slider page'</Text>
-        <Text style={styles.text}>{this.props.companies[this.props.currentCompany].name}</Text>
+        <Text style={styles.text}>{this.currentCompany().name}</Text>
+        <Slider
+          maximumValue={this.maxNumberOfShares()}
+          step={1}
+          onValueChange={(numberOfShares) => this.setState({numberOfShares: numberOfShares})} />
+        <Text>{'Number of shares: ' + this.state.numberOfShares}</Text>
+        <Text>{'Percentage of cash: ' + this.percentageOfCash()}</Text>
+        <Text>{'Cost: ' + this.cost()}</Text>
       </View>
     )
+  }
+
+  currentCompany() {
+    return this.props.companies[this.props.currentCompany]
+  }
+
+  cost() {
+    return (this.state.numberOfShares * this.currentCompany().price)
+  }
+
+  percentageOfCash() {
+    return (this.cost() / this.props.currentUser.cash) * 100
+  }
+
+  maxNumberOfShares() {
+    cash = this.props.currentUser.cash
+    price_per_share = this.currentCompany().price;
+    return (cash / price_per_share)
   }
 }
 
 function mapStateToProps(state) {
   return {
     companies: state.companies,
-    currentCompany: state.currentCompany
+    currentCompany: state.currentCompany,
+    currentUser: state.currentUser
   };
 }
 
--- src/reducers/reducer_current_user.js
+++ src/reducers/reducer_current_user.js
@@ -1,7 +1,7 @@
 export default function(state = null, action) {
   switch(action.type) {
   case 'USER_LOADED':
-    return action.payload;
+    return JSON.parse(action.payload);
   }
 
   return state;
